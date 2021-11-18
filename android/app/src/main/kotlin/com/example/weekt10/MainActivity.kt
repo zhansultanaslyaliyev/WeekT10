86bec9bfb8fab57751767c6975f4d8bd645ea7d0
Zhansultan <zhansultan.aslyaliyev@gmail.com>
2021-11-18 21:59:21 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/weekt10/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weekt10
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:weekt10/tasks_screen.dart';
+import 'package:weekt10/task_data.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => TaskData(),
+      child: MaterialApp(
+        home: TasksScreen(),
+      ),
+    );
+  }
+}
